2eed802505b30d00c865827d24b5a3a86992697c
keqing555 <1845441591@qq.com>
2021-10-11 22:04:53 +0800

    zookeeper

---
+++ src/test/java/Zookeeper_Test.java
@@ -0,0 +1,139 @@
+import org.apache.zookeeper.*;
+import org.apache.zookeeper.data.Stat;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.List;
+
+public class Zookeeper_Test {
+    private ZooKeeper zooKeeper;
+
+    /**
+     * 创建Zookeeper客户端连接
+     */
+    @Before
+    public void before() throws IOException {
+        zooKeeper = new ZooKeeper("192.168.212.129", 60000, new Watcher() {
+            //执行成功回调方法和监听回调方法
+            @Override
+            public void process(WatchedEvent watchedEvent) {
+                System.out.println("监听节点或数据变化时调用方法");
+                System.out.println("监听事件类型：" + watchedEvent.getType());
+                System.out.println("监听路径：" + watchedEvent.getPath());
+                //默认只监听一次，如果要多次监听，执行完监听方法回调方法后在此设置
+//                try {
+//                    zooKeeper.getChildren("/", true);
+//                } catch (KeeperException e) {
+//                    e.printStackTrace();
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+            }
+        });
+    }
+
+    /**
+     * 关闭连接
+     */
+    @After
+    public void After() {
+        try {
+            zooKeeper.close();
+            System.out.println("关闭连接！");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 创建节点
+     */
+    @Test
+    public void createNode() {
+        try {
+            zooKeeper.create("/tiwate", "提瓦特".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+            System.out.println("创建节点：tiwate");
+        } catch (KeeperException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 获取指定路径节点数据，并设置监听
+     */
+    @Test
+    public void getData() {
+        try {
+            byte[] data = zooKeeper.getData("/tiwate", true, null);
+            System.out.println("tiwate节点数据：");
+            System.out.println(new String(data));
+            Thread.sleep(1000000);
+            System.out.println("监听结束！");
+        } catch (KeeperException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 修改节点数据
+     */
+    @Test
+    public void setData() {
+        try {
+            zooKeeper.setData("/tiwate", "提瓦特大陆".getBytes(), -1);
+            System.out.println("节点提瓦特，已修改");
+        } catch (KeeperException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 获取指定路径下的子节点，并监听
+     */
+    @Test
+    public void getChildren() {
+        try {
+            List<String> children = zooKeeper.getChildren("/", true);
+            System.out.println("子节点数据:");
+            children.forEach(System.out::println);
+            Thread.sleep(1000000);
+            System.out.println("监听子节点结束");
+        } catch (KeeperException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 删除节点
+     */
+    @Test
+    public void deleteNode() {
+        try {
+            zooKeeper.delete("/tiwate", -1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (KeeperException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 判断节点是否存在，并监听该节点
+     */
+    @Test
+    public void isExist() throws InterruptedException, KeeperException {
+        Stat stat = zooKeeper.exists("/tiwate", true);
+        System.out.println(stat);
+        Thread.sleep(1000000);
+    }
+}
